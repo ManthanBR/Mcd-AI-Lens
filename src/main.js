1925dca2e5ffe6b115724509b6245975c4051ee4
ManthanBR <155616252+ManthanBR@users.noreply.github.com>
2025-04-03 22:55:35 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -49,7 +49,7 @@ import { Settings } from "./settings"
   })
   await session.setSource(source)
   source.setTransform(Transform2D.MirrorX)
-  await source.setRenderSize(window.innerWidth / 12, window.innerHeight / 12)
+  await source.setRenderSize(window.innerWidth, window.innerHeight)
   await session.setFPSLimit(Settings.camera.fps)
   await session.play()
 
