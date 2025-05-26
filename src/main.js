738813d01592863ab433f81bad071cc300e5b777
ManthanBR <155616252+ManthanBR@users.noreply.github.com>
2025-05-26 17:05:46 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -77,6 +77,7 @@ const source = createMediaStreamSource(mediaStream, {
     try {
       const source = await cameraManager.updateCamera(session)
       uiManager.updateRenderSize(source, liveRenderTarget)
+      await session.sendBehaviorEvent("onSwitchCamera")
     } catch (error) {
       console.error("Error switching camera:", error)
     }
