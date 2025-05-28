5f920aa604143c80544b4fa9070b6989759a9e7e
ManthanBR <155616252+ManthanBR@users.noreply.github.com>
2025-05-28 13:24:43 +0530

    update

--- src/recorder.js
+++ src/recorder.js
@@ -1,11 +1,9 @@
 import { Settings } from "./settings"
-import { fetchFile } from "@ffmpeg/util"
 
 export class MediaRecorderManager {
   constructor(videoProcessor, uiManager) {
     this.mediaRecorder = null
     this.recordedChunks = []
-    this.recordSegments = []
     this.videoProcessor = videoProcessor
     this.uiManager = uiManager
     this.audioVideoStream = null
@@ -25,14 +23,20 @@ export class MediaRecorderManager {
       this.recordedChunks = []
 
       this.mediaRecorder.ondataavailable = (event) => {
+        console.log("start record")
         if (event.data && event.data.size > 0) {
           this.recordedChunks.push(event.data)
         }
       }
 
       this.mediaRecorder.onstop = async () => {
+        console.log("stop record")
+        this.uiManager.showLoading(true)
         const blob = new Blob(this.recordedChunks, { type: Settings.recording.mimeType })
-        this.recordSegments.push(blob)
+        const fixedBlob = await this.videoProcessor.fixVideoDuration(blob)
+        const url = URL.createObjectURL(fixedBlob)
+        this.uiManager.showLoading(false)
+        this.uiManager.displayPostRecordButtons(url, fixedBlob)
       }
 
       this.mediaRecorder.start()
@@ -43,59 +47,29 @@ export class MediaRecorderManager {
     }
   }
 
-  stopRecording() {
-    if (this.mediaRecorder && this.mediaRecorder.state !== "inactive") {
-      this.mediaRecorder.stop()
-    }
-  }
-
-  async finalizeRecording() {
-    if (this.mediaRecorder && this.mediaRecorder.state !== "inactive") {
-      this.mediaRecorder.stop()
-    }
-
-    const mergedBlob = await this.mergeSegments()
-    this.uiManager.showLoading(false)
-    const url = URL.createObjectURL(mergedBlob)
-    this.uiManager.displayPostRecordButtons(url, mergedBlob)
-  }
-
-  async mergeSegments() {
-    if (this.recordSegments.length === 1) {
-      return this.recordSegments[0]
-    }
-
-    const ffmpeg = this.videoProcessor.ffmpeg
-    const { baseURL, coreURL, wasmURL, outputOptions } = Settings.ffmpeg
-
-    await ffmpeg.load({
-      coreURL: `${baseURL}/${coreURL}`,
-      wasmURL: `${baseURL}/${wasmURL}`,
-    })
-
-    for (let i = 0; i < this.recordSegments.length; i++) {
-      await ffmpeg.writeFile(`input${i}.mp4`, await fetchFile(this.recordSegments[i]))
-    }
-
-    const concatList = this.recordSegments.map((_, i) => `file 'input${i}.mp4'`).join('\n')
-    await ffmpeg.writeFile('concat.txt', concatList)
-
-    await ffmpeg.exec(['-f', 'concat', '-safe', '0', '-i', 'concat.txt', ...outputOptions, 'output.mp4'])
-    const output = await ffmpeg.readFile('output.mp4')
-    return new Blob([output.buffer], { type: Settings.recording.mimeType })
-  }
-
   resetRecordingVariables() {
     this.mediaRecorder = null
     this.recordedChunks = []
-    this.recordSegments = []
+    // Stop all tracks in the audio/video stream
     if (this.audioVideoStream) {
-      this.audioVideoStream.getTracks().forEach((track) => track.stop())
+      this.audioVideoStream.getTracks().forEach((track) => {
+        track.stop()
+      })
       this.audioVideoStream = null
     }
+
+    // Stop all tracks in the canvas stream
     if (this.canvasStream) {
-      this.canvasStream.getTracks().forEach((track) => track.stop())
+      this.canvasStream.getTracks().forEach((track) => {
+        track.stop()
+      })
       this.canvasStream = null
+    }
+  }
+
+  stopRecording() {
+    if (this.mediaRecorder && this.mediaRecorder.state !== "inactive") {
+      this.mediaRecorder.stop()
     }
   }
 }
